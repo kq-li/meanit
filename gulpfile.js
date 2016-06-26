994022eb5f80662070cd54299e81ed41dcf2d1c3
Kenneth Li <kennethli.3470@gmail.com>
2016-06-26 02:59:14 -0400

    Pug, gulp, less set up

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 var gulp = require('gulp');
 var pug = require('gulp-pug');
+var less = require('gulp-less');
+var path = require('path');
+var es = require('event-stream');
 
 gulp.task('default', function () {});
 
 gulp.task('views', function buildHTML() {
-  return gulp.src('./*.pug')
-    .pipe(pug())
-    .pipe(gulp.dest('./'));
+  return es.merge(gulp.src('./*.pug').pipe(pug()),
+                  gulp.src('./*.less').pipe(less({
+                    paths: [path.join(__dirname, 'less', 'includes')]
+                  }))).pipe(gulp.dest('./'));
 });
                                 
