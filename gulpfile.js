1ed260c908d16eaeb670b3d7ca6879a647082bab
Kenneth Li <kennethli.3470@gmail.com>
2016-07-26 21:00:00 -0400

    Added mongoose routes

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,17 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var pug = require('gulp-pug');
 var path = require('path');
 var es = require('event-stream');
 
 var lessPath = './public/less/*.less';
 var lessDest = './public/dist/';
+var pugPath = './views/*.pug';
+var pugDest = './views/';
 
 gulp.task('default', function () {});
 
-gulp.task('less', function() {
+gulp.task('less', function () {
   return gulp.src(lessPath)
     .pipe(less({
       paths: [path.join(__dirname, 'less', 'includes')]
@@ -16,6 +19,13 @@ gulp.task('less', function() {
     .pipe(gulp.dest(lessDest));
 });
 
+gulp.task('pug', function () {
+  return gulp.src(pugPath)
+    .pipe(pug())
+    .pipe(gulp.dest(pugDest));
+});
+
 gulp.task('watch', function () {
   gulp.watch([lessPath], ['less']);
+  gulp.watch([pugPath], ['pug']);
 });
--- public/js/app.js
+++ public/js/app.js
@@ -1,87 +1,135 @@
 var app = angular.module('meanit', ['ui.router']);
 
-app.factory('posts',
-            [function () {
-              var o = {
-                posts: [{title: 'post 1', upvotes: 5, comments: []},
-                        {title: 'post 2', upvotes: 12, comments: []},
-                        {title: 'post 3', upvotes: 9, comments: []},
-                        {title: 'post 4', upvotes: 1, comments: []},
-                        {title: 'post 5', upvotes: 3, comments: []}]
-              };
+app.factory('posts', [
+  '$http',
+  function ($http) {
+    var service = {
+      posts: []
+    };
 
-              return o;
-            }]);
+    service.getAllPosts = function () {
+      return $http.get('/posts').success(function (data) {
+        angular.copy(data, service.posts);
+      });
+    };
 
-app.controller('MainCtrl',
-               ['$scope',
-                'posts',
-                function ($scope, posts) {
-                  $scope.test = 'Hello World!';
-                  $scope.posts = posts.posts;
+    service.createPost = function (post) {
+      return $http.post('/posts', post).success(function (data) {
+        service.posts.push(data);
+      });
+    };
 
-                  $scope.addPost = function () {
-                    $scope.posts.push({
-                      title: $scope.title,
-                      link: $scope.link,
-                      upvotes: 0,
-                      comments: [{
-                        author: 'Ted',
-                        body: 'I love it',
-                        upvotes: 0
-                      }, {
-                        author: 'Alice',
-                        body: 'Amazing!',
-                        upvotes: 0
-                      }]
-                    });
-                    
-                    $scope.title = '';
-                    $scope.link = '';
-                  };
+    service.upvotePost = function (post) {
+      return $http.put('/posts/' + post._id + '/upvote').success(function (data) {
+        post.upvotes++;
+      });
+    };
 
-                  $scope.upvote = function (post) {
-                    post.upvotes++;
-                  };
-                }]);
+    service.getPost = function (id) {
+      return $http.get('/posts/' + id).then(function (res) {
+        return res.data;
+      });
+    };
 
-app.controller('PostsCtrl',
-               ['$scope',
-                '$stateParams',
-                'posts',
-                function ($scope, $stateParams, posts) {
-                  $scope.post = posts.posts[$stateParams.id];
+    service.addComment = function (id, comment) {
+      return $http.post('/posts/' + id + '/comments', comment);
+    };      
 
-                  $scope.addComment = function () {
-                    $scope.post.comments.push({
-                      author: $scope.author,
-                      body: $scope.body,
-                      upvotes: 0
-                    });
+    service.upvoteComment = function (id, comment) {
+      return $http
+        .put('/posts/' + id + '/comments/' + comment._id + '/upvote')
+        .success(function (data) {
+          comment.upvotes++;
+        });
+    };
+    
+    return service;
+  }
+]);
 
-                    $scope.author = '';
-                    $scope.body = '';
-                  };
-                  
-                  $scope.upvote = function (comment) {
-                    comment.upvotes++;
-                  };
-                }]);
+app.controller('MainCtrl', [
+  '$scope',
+  'posts',
+  function ($scope, posts) {
+    $scope.posts = posts.posts;
 
-app.config(['$stateProvider',
-            '$urlRouterProvider',
-            function ($stateProvider, $urlRouterProvider) {
-              $stateProvider
-                .state('home', {
-                  url: '/home',
-                  templateUrl: '/views/home.html',
-                  controller: 'MainCtrl'
-                })
-                .state('posts', {
-                  url: '/posts/{id}',
-                  templateUrl: '/views/posts.html',
-                  controller: 'PostsCtrl'
-                });
+    $scope.addPost = function () {
+      if (!$scope.title || $scope.title === '')
+        return;
 
-              $urlRouterProvider.otherwise('home');
-            }]);
+      posts.createPost({
+        title: $scope.title,
+        link: $scope.link
+      });
+      
+      $scope.title = '';
+      $scope.link = '';
+    };
+
+    $scope.upvote = function (post) {
+      posts.upvotePost(post);
+    };
+  }
+]);
+
+app.controller('PostsCtrl', [
+  '$scope',
+  'posts',
+  'post',
+  function ($scope, posts, post) {
+    $scope.post = post;
+    
+    $scope.addComment = function () {
+      posts.addComment(post._id, {
+        author: $scope.author,
+        body: $scope.body
+      }).success(function (comment) {
+        $scope.post.comments.push(comment);
+      });
+      
+      $scope.author = '';
+      $scope.body = '';
+    };
+    
+    $scope.upvote = function (comment) {
+      posts.upvoteComment(post._id, comment);
+    };
+  }
+]);
+
+app.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: '/views/home.html',
+        controller: 'MainCtrl',
+        resolve: {
+          postPromise: [
+            'posts',
+            function (posts) {
+              return posts.getAllPosts();
+            }
+          ]
+        }                      
+      })
+      .state('posts', {
+        url: '/posts/{id}',
+        templateUrl: '/views/posts.html',
+        controller: 'PostsCtrl',
+        resolve: {
+          post: [
+            '$stateParams',
+            'posts',
+            function ($stateParams, posts) {
+              return posts.getPost($stateParams.id);
+            }
+          ]
+        }
+      });
+    
+    $urlRouterProvider.otherwise('home');
+  }
+]);
